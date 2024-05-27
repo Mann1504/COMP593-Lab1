20a89d6e18d3f3e2e463462e15f7e42da7fc122a
Mann1504 <mannhp@flemingcollege.ca>
2024-05-27 13:36:56 -0400

    Modified both files in the repo clone
    - Deleted a print() statement from script1.py
    -Added two print() statement to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Print lines from Zen of Python
 print("sparse is better than dense.")
-print("Flat is better than nested")
--- script2.py
+++ script2.py
@@ -1,2 +1,4 @@
 # This is good programing advice
 print("readability counts.")
+print("Unless explicity silenced")
+print("Simple is better than complax.")
